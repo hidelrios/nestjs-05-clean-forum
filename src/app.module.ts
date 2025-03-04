8e990471d3d72c7e9962e57648ef869febb60fdc
hidelrios <hidels.rios@gmail.com>
2025-03-04 15:27:29 -0300

    fundamentos nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service';
+import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module'
+import { AuthenticationController } from './controllers/authenticate.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [AuthenticationController, CreateAccountController, CreateQuestionController, FetchRecentQuestionsController],
+  providers: [PrismaService],
+})
+export class AppModule { }
